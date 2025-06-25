e3675a59314b1a97494d7b6869c64496683f459b
SamSchmakel28 <samuel.schmakel@gmail.com>
2025-06-25 14:08:12 -0500

    Added get_file_content() function

--- tests.py
+++ tests.py
@@ -1,24 +1,19 @@
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 
 def test():
-    result = get_files_info("calculator", ".")
+    result = get_file_content("calculator", "main.py")
     print("Result for current directory:")
     print(result)
     print("")
 
-    result = get_files_info("calculator", "pkg")
+    result = get_file_content("calculator", "pkg/calculator.py")
     print("Result for 'pkg' directory:")
     print(result)
 
-    result = get_files_info("calculator", "/bin")
+    result = get_file_content("calculator", "/bin/cat")
     print("Result for '/bin' directory:")
     print(result)
-
-    result = get_files_info("calculator", "../")
-    print("Result for '../' directory:")
-    print(result)
-
 
 if __name__ == "__main__":
     test()
