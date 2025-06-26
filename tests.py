67cc96f18643eaf087eed745111732d148a3f0a7
SamSchmakel28 <samuel.schmakel@gmail.com>
2025-06-25 19:46:48 -0500

    Added write_file() function

--- tests.py
+++ tests.py
@@ -1,18 +1,19 @@
-from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 
 def test():
-    result = get_file_content("calculator", "main.py")
-    print("Result for current directory:")
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    print("Result:")
     print(result)
     print("")
 
-    result = get_file_content("calculator", "pkg/calculator.py")
-    print("Result for 'pkg' directory:")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    print("Result:")
     print(result)
+    print("")
 
-    result = get_file_content("calculator", "/bin/cat")
-    print("Result for '/bin' directory:")
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    print("Result:")
     print(result)
 
 if __name__ == "__main__":
