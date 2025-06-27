5c05fcf9df29ab12d52fc822b0e96497237a573b
SamSchmakel28 <samuel.schmakel@gmail.com>
2025-06-27 02:32:18 -0500

    Added config to generate_content() function call

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
+from prompts import system_prompt
 
 def main():
     load_dotenv()
@@ -12,8 +13,8 @@ def main():
 
     if not args:
         print("AI Code Assistant")
-        print('\nUsage: python main.py "your prompt here"')
-        print('Example: python main.py "How do I build a calculator app?"')
+        print('\nUsage: python3 main.py "your prompt here"')
+        print('Example: python3 main.py "How do I fix the calculator?"')
         sys.exit(1)
 
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -32,7 +33,8 @@ def main():
 def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
-        contents=messages
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
 
     if verbose:
