7c55280e2c2b4b5709b42c6a90aa2c607890df46
SamSchmakel28 <samuel.schmakel@gmail.com>
2025-06-26 20:05:38 -0500

    Created run_python.py function

---
+++ functions/run_python.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path, args=None):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(abs_file_path):
+        return f'Error: File "{file_path}" not found.'
+    if not file_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+    try:
+        cmds = ["python3", abs_file_path]
+        if args:
+            cmds.extend(args)
+        result = subprocess.run(
+        cmds,
+        cwd=abs_working_dir,
+        capture_output=True,
+        text=True, # treates stdout and stderr as strings instead of bytes
+        timeout=30 # timeout after 30 seconds
+        )
+        output = ""
+        if result.stdout:
+            output += f"STDOUT:\n{result.stdout}"
+        if result.stderr :
+            output += f"STDERR:\n{result.stderr}"
+        if result.returncode != 0:
+            output += f"\nProcess exited with code {result.returncode}"
+        if result.stdout == "" and result.stderr == "":
+            return "No output produced"
+        return output if output else "No output produced"
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
+    
--- tests.py
+++ tests.py
@@ -1,18 +1,22 @@
-from functions.write_file import write_file
+from functions.run_python import run_python_file
 
 
 def test():
-    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    result = run_python_file("calculator", "main.py")
     print("Result:")
     print(result)
     print("")
 
-    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    result = run_python_file("calculator", "tests.py")
     print("Result:")
     print(result)
     print("")
 
-    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    result = run_python_file("calculator", "../main.py")
+    print("Result:")
+    print(result)
+
+    result = run_python_file("calculator", "nonexistent.py")
     print("Result:")
     print(result)
 
