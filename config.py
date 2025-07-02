1d8a76ff1577cee3e8f16da003253b415032115c
SamSchmakel28 <samuel.schmakel@gmail.com>
2025-07-01 21:21:09 -0500

    Added feedback loop to allow iterative processes

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 MAX_CHARS = 10000
 WORKING_DIRECTORY = "./calculator"
+MAX_ITERS = 20
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from google.genai import types
 from dotenv import load_dotenv
 from prompts import system_prompt
 from call_function import available_functions, call_function
+from config import MAX_ITERS
 
 def main():
     load_dotenv()
@@ -29,9 +30,24 @@ def main():
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
-    generate_content(client, messages, verbose)
+
+    iters = 0
+    while True:
+        iters += 1
+        if iters > MAX_ITERS:
+            print(f"Maximum iterations ({MAX_ITERS}) reached.")
+            sys.exit(1)
+        try:
+            final_response = generate_content(client, messages, verbose)
+            if final_response:
+                print("Final response:")
+                print(final_response)
+                break
+        except Exception as e:
+            print(f"Error in generate_content: {e}")
 
 def generate_content(client, messages, verbose):
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
         contents=messages,
@@ -44,17 +60,31 @@ def generate_content(client, messages, verbose):
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     
+    if response.candidates:
+        for candidate in response.candidates:
+            function_call_content = candidate.content
+            messages.append(function_call_content)
+
     if not response.function_calls:
         return response.text
     
+    function_responses = []
     for function_call_part in response.function_calls:
         function_call_result = call_function(function_call_part, verbose)
-        if not function_call_result.parts[0].function_response.response:
-            raise Exception("No response found for function call")
-        else:
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response.response
+        ):
+            raise Exception("empty function call result")
+        if verbose:
             print(f"-> {function_call_result.parts[0].function_response.response}")
-            if function_call_part.name == "write_file":
-                print(f"\n Content: hello")
+        function_responses.append(function_call_result.parts[0])
+    
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
+    
+    messages.append(types.Content(role="tool", parts=function_responses))
+    
 
 if __name__ == "__main__":
     main()
